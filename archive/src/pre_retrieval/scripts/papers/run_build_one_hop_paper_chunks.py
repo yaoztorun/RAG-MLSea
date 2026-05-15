964ce46ed21978d4dfa146503dfb9e8ba1d2d209
yaoztorun <alpyigitoztorun@gmail.com>
2026-05-15 13:28:42 +0200

    flag before enhancing the ML Question Dataset from 280 to 500

--- archive/src/pre_retrieval/scripts/papers/run_build_one_hop_paper_chunks.py
+++
@@ -1,5 +0,0 @@
-from src.pre_retrieval.chunking.papers.build_one_hop_paper_chunks import main
-
-
-if __name__ == "__main__":
-    main()
--- archive/src/pre_retrieval/scripts/papers/run_build_predicate_filtered_chunks.py
+++
@@ -1,5 +0,0 @@
-from src.pre_retrieval.chunking.papers.build_predicate_filtered_chunks import main
-
-
-if __name__ == "__main__":
-    main()
